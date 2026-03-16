b9d11d3ed812fda15f0f6a2fa984525bf2b88dff
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-03-16 09:47:56 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,12 @@
+def solution(rsp):
+    answer = ''
+    
+    for num in rsp:
+        if num == "2":
+            answer += "0"
+        elif num == "0":
+            answer += "5"
+        else:
+            answer += "2"
+            
+    return answer
