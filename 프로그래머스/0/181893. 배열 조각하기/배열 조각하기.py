019cc6d684340381ac513837b8b9f7e79050f7a3
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-04-21 10:20:27 +0900

    [level 0] Title: 배열 조각하기, Time: 1.31 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.py
@@ -0,0 +1,12 @@
+def solution(arr, query):
+    answer = arr
+    
+    for i,n in enumerate(query):
+        idx = query[i]
+        
+        if i%2 == 0:       
+            answer = answer[:idx+1]
+        else:
+            answer = answer[idx:]
+        
+    return answer
