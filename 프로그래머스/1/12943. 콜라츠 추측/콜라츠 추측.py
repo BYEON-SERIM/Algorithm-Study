4a5c6234480b3e8e8ddfde955642f1cba41cfbaf
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-04-21 10:31:56 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,17 @@
+def solution(num):
+    answer = 0
+    
+    n = num
+    
+    while(n > 1):
+        if answer >= 500:
+            return -1
+    
+        if n%2 == 0:
+            n = n//2
+        else:
+            n = n*3+1
+        
+        answer += 1
+    
+    return answer 
