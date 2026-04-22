0845dc38bb8db680d9c0de7efc6f3389011f784a
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-04-22 15:07:35 +0900

    [level 2] Title: 짝지어 제거하기, Time: 142.32 ms, Memory: 15.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    
+    stack = []
+    
+    for c in s:
+        if len(stack) == 0:
+            stack.append(c)
+        else:
+            if stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)                
+    
+    return 1 if len(stack) == 0 else 0
