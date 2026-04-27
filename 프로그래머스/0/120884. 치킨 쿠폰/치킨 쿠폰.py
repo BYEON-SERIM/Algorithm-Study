895751c4febaeb89e1e0b50a7dab1b01222263d0
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-04-27 16:48:19 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,11 @@
+def solution(chicken):
+    answer = 0
+    coupons = chicken
+    
+    while coupons >= 10:
+        service = coupons // 10
+        answer += service
+        
+        coupons = (coupons % 10) + service
+        
+    return answer
