a9586a0c8bfecd03d641976503829f0629c64274
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-05-27 17:00:18 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,8 @@
 def solution(absolutes, signs):
     answer = 0
-    
-    for a, s in zip(absolutes, signs):
-        if s == True:
+
+    for a,s in zip(absolutes,signs):
+        if s:
             answer += a
         else:
             answer -= a
