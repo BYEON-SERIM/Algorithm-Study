46990211b27a727c8e2f0275b60bce5a578c8d9f
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-05-28 09:59:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,7 +1,3 @@
 def solution(phone_number):
-    answer = ''
-    
-    for i in range(1,len(phone_number) - 3):
-        answer += "*"
-    
-    return answer + phone_number[-4:]
+
+    return len(phone_number[:-4])*'*' + phone_number[-4:]
