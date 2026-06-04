ca4e43700e937a08d181bdde6654fad3b6cba9ed
BYEON-SERIM <100740095+BYEON-SERIM@users.noreply.github.com>
2026-06-04 09:51:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 13.13 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,16 +1,16 @@
 def solution(left, right):
     answer = 0
     
-    for i in range(left,right+1):
-        cnt = 0
+    for i in range(left, right+1):
+        chk = 0
+            
+        for j in range(1, i+1):
+            if i%j == 0:
+                chk += 1
         
-        for n in range(1,i+1):
-            if i%n == 0:
-                cnt+=1
-        
-        if cnt%2 == 0:
+        if chk%2 == 0:
             answer += i
         else:
             answer -= i
-    
+        
     return answer
